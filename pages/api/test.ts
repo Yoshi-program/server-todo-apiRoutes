6ae0967938fcad91434e54b4ad5c20f3af05ec27
s1F102103303 <s1F102103303@iniad.org>
2022-11-11 22:40:23 +0900

    curlコマンドで受け取ったTodoリストの情報をデータベースに保存

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,45 +1,25 @@
-import type { Prisma, User } from '@prisma/client'
+import type { Prisma, ToDoList } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 
-// type TestRes = {
-//   title: string
-//   content: string
-// }
+type ToDo = {
+  content?: string
+}
 
-// type Users = {
-//   name: string
-// }
-
-const handler = async (req: NextApiRequest, res: NextApiResponse<User | { error: string }>) => {
-  // const { content }: ToDo = req.body
-  // const result: TestRes = {
-  //   title: `id: ${req.query.id}, authorization header: ${req.headers.authorization}`,
-  //   content: `body[test]: ${req.body['test']}, cookie[test]: ${req.cookies.test}`,
-  // }
-  // const feed = await prisma.post.findMany({
-  //   where: { published: true },
-  //   include: {
-  //     author: {
-  //       select: { name: true },
-  //     },
-  //   },
-  // })
-  const userBody: Prisma.UserCreateInput = {
-    name: 'Yoshi',
+const handler = async (req: NextApiRequest, res: NextApiResponse<ToDoList | { error: string }>) => {
+  const { content }: ToDo = req.body
+  const todoBody: Prisma.ToDoListCreateInput = {
+    content: content,
   }
-  // userBody = {
-  //   name: 'Yoshi',
-  // }
   let statusCode = 200
-  const result = await prisma.user
+  const result = await prisma.toDoList
     .create({
-      data: userBody,
+      data: todoBody,
     })
     .catch((err) => {
       statusCode = 500
       console.log(err)
-      return { error: 'Failed to read user' }
+      return { error: 'Failed to read content' }
     })
     .finally(async () => {
       await prisma.$disconnect()
@@ -47,14 +27,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<User | { error:
 
   res.status(statusCode).json(result)
 }
-// console.log('つながった！！')
-// console.log(req.body.name)
-// console.log(req.body.age)
-// const response = await fetch('https://jsonplaceholder.typicode.com/users/')
-// const users = await response.json()
-// res.status(200).send(users)
 
 export default handler
 
 // ターミナルに打つコマンド
-// curl -X POST -H "Content-Type: application/json" -d '{"do":"課題をやる"}' http://localhost:3000/api/test/
+// curl -X POST -H "Content-Type: application/json" -d '{"content":"課題をやる"}' http://localhost:3000/api/test/
